257d6985c3ddc2041e9ee856fca1afa173e225da
sanshinan <2274682830@qq.com>
2020-12-15 17:20:13 +0800

    新的提交

--- app/src/main/java/com/example/day01/MainActivity.java
+++ app/src/main/java/com/example/day01/MainActivity.java
@@ -1,19 +1,29 @@
 package com.example.day01;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TextView text;
+    private ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView viewById = findViewById(R.id.text);
-        viewById.setText("Android 4.1.1");
 
+
+        initView();
+    }
+
+    private void initView() {
+        text = (TextView) findViewById(R.id.text);
+        img = (ImageView) findViewById(R.id.img);
+        text.setText("Android 4.1.1版本");
+        img.setImageResource(R.mipmap.ic_launcher);
     }
 }
