fda0bef60facd1abf875dd63e56241650ac5457b
sanshinan <2274682830@qq.com>
2020-12-15 15:25:07 +0800

    fly

---
+++ app/src/main/java/com/example/day01/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.day01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView viewById = findViewById(R.id.text);
+        viewById.setText("Android 4.1.1");
+
+    }
+}
